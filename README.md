e066bcd3ed61a9b52f0a351a1f6b088e71c4d191
Patrick Michaelsen <michaelsenpatrick@gmail.com>
2017-04-05 17:31:53 -0700

    finding dependencies

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ mvn exec:java
 
 #Clean Build Install
 ./mvnw clean package
+
+
+More maven help:
+http://www.vogella.com/tutorials/ApacheMaven/article.html
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,13 @@ package com.mycompany.app;
  * Hello world!
  *
  */
+import example.server.MinimalRestServer;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+				MinimalRestServer.main( args );
     }
 }
